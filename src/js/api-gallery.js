73c57a1d57706c4888240abb6fa96760c1859281
Olha Tarasova <tarasovao0505@gmail.com>
2021-09-06 20:13:27 +0300

    Жанры

--- src/js/api-gallery.js
+++ src/js/api-gallery.js
@@ -1,4 +1,11 @@
 import templateGalleryFilms from '../templates/films-gallery.hbs';
+import genresJson from './genres.json';
+import genreCard from '../templates/genre-card.hbs';
+
+function forGenres() {
+  const { genres } = genresJson;
+  return genres;
+}
 
 const galleryEl = document.querySelector('.gallery-list');
 
@@ -12,10 +19,13 @@ class NewApiService {
 
     return fetch(url)
       .then(response => response.json())
-      .then(data => {
+      .then(({ results }) => {
+        //fetchGenres().then(({ genres }) => {
+        //  updateMovieMarkup(results, genres);
+        // });
         this.incrementPage();
-        console.log(data.results);
-        return data.results;
+        console.log(results);
+        return results;
       });
   }
 
@@ -34,18 +44,109 @@ class NewApiService {
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
+
+  //fetchGenres() {
+  //return fetch(
+  //  `https://api.themoviedb.org/3/genre/movie/list?api_key=b32f977d148061c9ab22a471ff2c7792&page=${this.page}`,
+  // ).then(response => response.json());
+  //}
+
+  // updateMovieMarkup(films, genres) {
+  //  films.map(({ id, poster_path, title, release_date, genre_ids }) => {
+  //  const filterGenres = genres.filter(genre => genre_ids.includes(genre.id));
+  //  const mapGenres = filterGenres.map(({ name }) => name);
+  //  if (mapGenres.length > 3) {
+  //    mapGenres.splice(3, 0, 'Other');
+  //  }
+  // const movieGenres = mapGenres.slice(0, 4).join(', ');
+  //  const releaseDate = release_date.split('-'[0]);
+
+  //  let img = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : emptyJpg;
+
+  // const movie = [{ id, img, title, movieGenres, releaseDate }];
+  // updateMarkup(movie);
+  // console.log(movie);
+  // });
+  //}
 }
+
+//function updateMarkup(movie) {
+//newApiService.fetchArticles().then(() => {
+//  let markup = '';
+//  if (movie.status !== '404') {
+//    markup = genreCard(movie);
+//   }
+//  galleryEl.insertAdjacentHTML('beforeend', markup);
+// });
+// }
 
 // Функция для отрисовки фильмов через template  в HTML
 
 document.addEventListener('DOMContentLoaded', renderCardGallery);
 
+//document.addEventListener('DOMContentLoaded', updateMarkup);
+//document.addEventListener('DOMContentLoaded', bossPage);
+
 const newApiService = new NewApiService();
 
 function renderCardGallery(results) {
-  newApiService.fetchArticles().then(img => {
-    const markup = templateGalleryFilms(img);
+  newApiService.fetchArticles().then(results => {
+    const markup = templateGalleryFilms(results);
     galleryEl.insertAdjacentHTML('beforeend', markup);
+    updateMovieMarkup();
+  });
+}
+
+//function renderCardGallery(results) {
+// const markup = templateGalleryFilms(results);
+// galleryEl.insertAdjacentHTML('beforeend', markup);
+
+//}
+
+//async function bossPage() {
+// const data = await newApiService.fetchArticles();
+// console.log(data);
+
+//const movies = data.results;
+//console.log(movies);
+
+//const genresStart = forGenres();
+//const bigTrend = trendFilms(movies, genresStart);
+//renderCardGallery(bigTrend);
+//console.log(results);
+//}
+
+//function createGenresFromTrend(array) {
+// return array.genres
+//  .map(genres => genres.name)
+//  .slice(0, 3)
+//   .flat();
+//}
+
+// Слияние полной информации о фильме для трендов
+
+//function trendFilms(films, genresJson) {
+// return films.map(film => ({
+//   ...film,
+//   genres: createGenresFromTrend(film.genre_ids, genresJson),
+// }));
+//}
+
+function updateMovieMarkup(films, genres) {
+  films.map(({ id, poster_path, title, release_date, genre_ids }) => {
+    const filterGenres = genres.filter(genre => genre_ids.includes(genre.id));
+    const mapGenres = filterGenres.map(({ name }) => name);
+    if (mapGenres.length > 3) {
+      mapGenres.splice(3, 0, 'Other');
+    }
+    const movieGenres = mapGenres.slice(0, 4).join(', ');
+    const releaseDate = release_date.slice(0, 4);
+
+    let img = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : emptyJpg;
+
+    const movie = [{ id, img, title, movieGenres, releaseDate }];
+
+    console.log(movie);
   });
 }
 
