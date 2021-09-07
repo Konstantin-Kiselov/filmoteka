8a9ed0cdcab48a0435078eb9e2a74c974c3f28df
Olha Tarasova <tarasovao0505@gmail.com>
2021-09-07 20:34:19 +0300

    Gallery

--- src/js/api-gallery.js
+++ src/js/api-gallery.js
@@ -2,10 +2,10 @@ import templateGalleryFilms from '../templates/films-gallery.hbs';
 import genresJson from './genres.json';
 import genreCard from '../templates/genre-card.hbs';
 
-// function forGenres() {
+//function forGenres() {
 const { genres } = genresJson;
 // return genres;
-// }
+//}
 
 const galleryEl = document.querySelector('.gallery-list');
 
@@ -20,9 +20,9 @@ class NewApiService {
     return fetch(url)
       .then(response => response.json())
       .then(({ results }) => {
-        //fetchGenres().then(({ genres }) => {
-        //  updateMovieMarkup(results, genres);
-        // });
+        fetchGenres().then(({ genres }) => {
+          markupMovieFilm(results, genres);
+        });
         this.incrementPage();
         console.log(results);
         return results;
@@ -44,41 +44,20 @@ class NewApiService {
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
-
-  //fetchGenres() {
-  //return fetch(
-  //  `https://api.themoviedb.org/3/genre/movie/list?api_key=b32f977d148061c9ab22a471ff2c7792&page=${this.page}`,
-  // ).then(response => response.json());
-  //}
-
-  // updateMovieMarkup(films, genres) {
-  //  films.map(({ id, poster_path, title, release_date, genre_ids }) => {
-  //  const filterGenres = genres.filter(genre => genre_ids.includes(genre.id));
-  //  const mapGenres = filterGenres.map(({ name }) => name);
-  //  if (mapGenres.length > 3) {
-  //    mapGenres.splice(3, 0, 'Other');
-  //  }
-  // const movieGenres = mapGenres.slice(0, 4).join(', ');
-  //  const releaseDate = release_date.split('-'[0]);
-
-  //  let img = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : emptyJpg;
-
-  // const movie = [{ id, img, title, movieGenres, releaseDate }];
-  // updateMarkup(movie);
-  // console.log(movie);
-  // });
-  //}
 }
 
-//function updateMarkup(movie) {
-//newApiService.fetchArticles().then(() => {
-//  let markup = '';
-//  if (movie.status !== '404') {
-//    markup = genreCard(movie);
-//   }
-//  galleryEl.insertAdjacentHTML('beforeend', markup);
-// });
-// }
+function fetchGenres() {
+  return fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=b32f977d148061c9ab22a471ff2c7792`,
+  ).then(response => response.json());
+  // .then(gen => {
+  //   genres = gen.genres;
+  //   console.log(gen);
+  //   fetchArticles();
+  // });
+}
+
+//fetchGenres();
 
 // Функция для отрисовки фильмов через template  в HTML
 
@@ -89,66 +68,76 @@ document.addEventListener('DOMContentLoaded', renderCardGallery);
 
 const newApiService = new NewApiService();
 
-function renderCardGallery(results) {
-  newApiService.fetchArticles().then(results => {
-    const markup = templateGalleryFilms(results);
-    galleryEl.insertAdjacentHTML('beforeend', markup);
-    updateMovieMarkup(results, genres);
-  });
+function renderCardGallery(results, genres) {
+  newApiService
+    .fetchArticles()
+    .markupMovieFilm()
+    .then(results => {
+      const markup = genreCard(results);
+      galleryEl.insertAdjacentHTML('beforeend', markup);
+    });
 }
 
-//function renderCardGallery(results) {
-// const markup = templateGalleryFilms(results);
-// galleryEl.insertAdjacentHTML('beforeend', markup);
+// function renderCollection(results) {
+//   const markup = templateGalleryFilms(results);
+//   galleryEl.insertAdjacentHTML('beforeend', markup);
+// }
 
-//}
+// async function bossPage() {
+//   const data = await newApiService.fetchArticles();
+//   console.log(data);
 
-//async function bossPage() {
-// const data = await newApiService.fetchArticles();
-// console.log(data);
+//   const movies = data.results;
+//   console.log(movies);
 
-//const movies = data.results;
-//console.log(movies);
+//   const genresStart = forGenres();
+//   const bigTrend = trendFilms(movies, genresStart);
+//   renderCollection(bigTrend);
+//   console.log(results);
+// }
 
-//const genresStart = forGenres();
-//const bigTrend = trendFilms(movies, genresStart);
-//renderCardGallery(bigTrend);
-//console.log(results);
-//}
+// function createGenresFromTrend(array, genres) {
+//   return array
+//     .map(id => genres.filter(element => element.id === id))
+//     .slice(0, 3)
+//     .flat();
+// }
 
-//function createGenresFromTrend(array) {
-// return array.genres
-//  .map(genres => genres.name)
-//  .slice(0, 3)
-//   .flat();
-//}
+// // Слияние полной информации о фильме для трендов
 
-// Слияние полной информации о фильме для трендов
+// function trendFilms(films, genresJson) {
+//   return films.map(film => ({
+//     ...film,
+//     genres: createGenresFromTrend(film.genre_ids, genresJson),
+//   }));
+// }
 
-//function trendFilms(films, genresJson) {
-// return films.map(film => ({
-//   ...film,
-//   genres: createGenresFromTrend(film.genre_ids, genresJson),
-// }));
-//}
-
-function updateMovieMarkup(films, genres) {
-  films.map(({ id, poster_path, title, release_date, genre_ids }) => {
+function markupMovieFilm(results, genres) {
+  results.map(({ id, poster_path, title, release_date, genre_ids, vote_average }) => {
     const filterGenres = genres.filter(genre => genre_ids.includes(genre.id));
     const mapGenres = filterGenres.map(({ name }) => name);
     if (mapGenres.length > 3) {
       mapGenres.splice(3, 0, 'Other');
     }
-    const movieGenres = mapGenres.slice(0, 4).join(', ');
-    const releaseDate = release_date.slice(0, 4);
+    const filmGenres = mapGenres.slice(0, 4).join(', ');
+    const releaseYear = release_date.slice(0, 4);
 
     let img = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : emptyJpg;
 
-    const movie = [{ id, img, title, movieGenres, releaseDate }];
-
-    console.log(movie);
+    const movie = [{ id, img, title, filmGenres, releaseYear, vote_average }];
+    updateMarkup(movie);
     return movie;
   });
+}
+
+function updateMarkup(movie) {
+  let markup = '';
+
+  if (movie.status !== '404') {
+    markup = genreCard(movie);
+  }
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  //});
 }
 
 export { galleryEl, newApiService, renderCardGallery };
