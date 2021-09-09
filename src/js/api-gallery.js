5893b67fb600aef2cbd9c6e91918f3bef465a4e8
Olha Tarasova <tarasovao0505@gmail.com>
2021-09-09 15:03:16 +0300

    localStorage

--- src/js/api-gallery.js
+++ src/js/api-gallery.js
@@ -1,11 +1,7 @@
 import templateGalleryFilms from '../templates/films-gallery.hbs';
 import genresJson from './genres.json';
 import genreCard from '../templates/genre-card.hbs';
-
-//function forGenres() {
-// const { genres } = genresJson;
-// return genres;
-//}
+const STORAGE_KEY = 'genres';
 
 const galleryEl = document.querySelector('.gallery-list');
 
@@ -20,11 +16,7 @@ class NewApiService {
     return fetch(url)
       .then(response => response.json())
       .then(({ results }) => {
-        // fetchGenres().then(({ genres }) => {
-        //   markupMovieFilm(results, genres);
-        // });
         this.incrementPage();
-        // console.log(results);
         return results;
       });
   }
@@ -46,25 +38,16 @@ class NewApiService {
   }
 }
 
-//fetchGenres();
-
-// Функция для отрисовки фильмов через template  в HTML
+// // Функция для отрисовки фильмов через template  в HTML
 
 document.addEventListener('DOMContentLoaded', renderCardGallery);
-
-//document.addEventListener('DOMContentLoaded', updateMarkup);
-//document.addEventListener('DOMContentLoaded', bossPage);
 
 const newApiService = new NewApiService();
 
 function renderCardGallery(results, genres) {
   newApiService.fetchArticles().then(results => {
     fetchGenres().then(genres => {
-      // console.log(genres);
-      // console.log(results);
       markupMovieFilm(results, genres);
-      // const markup = genreCard(results);
-      // galleryEl.insertAdjacentHTML('beforeend', markup);
     });
   });
 }
@@ -75,55 +58,26 @@ function fetchGenres() {
   )
     .then(response => response.json())
     .then(({ genres }) => {
-      // console.log(genres);
-      // markupMovieFilm(results, genres);
+      console.log(genres);
+      const storageGenres = JSON.stringify(genres);
+      localStorage.setItem(STORAGE_KEY, storageGenres);
+      const getLocalGenres = localStorage.getItem(STORAGE_KEY);
+      const localGenres = JSON.parse(getLocalGenres);
+      console.log(localGenres);
       return genres;
     });
 }
 
-// function renderCollection(results) {
-//   const markup = templateGalleryFilms(results);
-//   galleryEl.insertAdjacentHTML('beforeend', markup);
-// }
-
-// async function bossPage() {
-//   const data = await newApiService.fetchArticles();
-//   console.log(data);
-
-//   const movies = data.results;
-//   console.log(movies);
-
-//   const genresStart = forGenres();
-//   const bigTrend = trendFilms(movies, genresStart);
-//   renderCollection(bigTrend);
-//   console.log(results);
-// }
-
-// function createGenresFromTrend(array, genres) {
-//   return array
-//     .map(id => genres.filter(element => element.id === id))
-//     .slice(0, 3)
-//     .flat();
-// }
-
-// // Слияние полной информации о фильме для трендов
-
-// function trendFilms(films, genresJson) {
-//   return films.map(film => ({
-//     ...film,
-//     genres: createGenresFromTrend(film.genre_ids, genresJson),
-//   }));
-// }
-
 function markupMovieFilm(results, genres) {
   results.map(({ id, poster_path, title, release_date, genre_ids, vote_average }) => {
-    console.log(genres);
     const filterGenres = genres.filter(genre => genre_ids.includes(genre.id));
+
     const mapGenres = filterGenres.map(({ name }) => name);
     if (mapGenres.length > 3) {
       mapGenres.splice(3, 0, 'Other');
     }
     const filmGenres = mapGenres.slice(0, 4).join(', ');
+
     const releaseYear = release_date.slice(0, 4);
 
     let img = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : emptyJpg;
@@ -141,7 +95,43 @@ function updateMarkup(movie) {
     markup = genreCard(movie);
   }
   galleryEl.insertAdjacentHTML('beforeend', markup);
-  //});
 }
+
+/////////////    "Это будет второй вариант"
+
+// function addGenres() {
+//   localStorage.setItem(STORAGE_KEY, filmGenres);
+//   const saveGenres = localStorage.getItem(STORAGE_KEY);
+
+//   console.log(saveGenres);
+
+//   // if (localStorage.getItem(STORAGE_KEY) === null) {
+//   //   localStorage.setItem(STORAGE_KEY, JSON.stringify(arrWatchedFilms));
+//   // }
+// }
+// addGenres();
+
+//let arrGenresFilms = [];
+
+// Функцтя для жанров проба
+
+// function markupGenres(genres) {
+//   genres.map(({ genres }) => {
+//     arrGenresFilms.push(genres);
+//   });
+//   console.log(markupGenres);
+// }
+// const storageGenres = JSON.stringify(genres); // genres это массив объектов, который приходит после фетча
+// localStorage.setItem('genres', storageGenres);
+
+// function getArrWatchedFilms() {
+//   // получает масив фильмов добавленных в  из локал сторедж
+//   if (localStorage.getItem(STORAGE_KEY)) {
+//     const arrString = localStorage.getItem(STORAGE_KEY);
+//     const arrPars = JSON.parse(arrString);
+//     return (arrWatchedFilms = [...arrPars]);
+//   }
+//   return [];
+// }
 
 export { galleryEl, newApiService, renderCardGallery };
