3b000207a52383ccfd59c06dc4c8593897f97dfe
Serg_Grish <balchyg@icloud.com>
2021-09-10 21:16:38 +0300

    55

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,4 +1,5 @@
 import headerTemplates from '../templates/header-tpl.hbs';
+<<<<<<< Updated upstream
 // import newApiService from './api-gallery';
 import genre2 from '../templates/genre2-card.hbs';
 
@@ -19,13 +20,27 @@ function updateHeaderMarkup() {
   refs.header.insertAdjacentHTML('afterbegin', headerTemplates());
   refs.libraryBtn.addEventListener('click', libraryHandleClick);
 }
+=======
+
+const refs = {
+  header: document.querySelector('header'),
+  filmsGallery: document.querySelector('#films-gallery'),
+  homeLink: document.querySelector('.navigation__link__home'),
+  libraryBtn: document.querySelector('.navigation__link__library'),
+};
+
+refs.libraryBtn.addEventListener('click', libraryHandleClick);
+>>>>>>> Stashed changes
 
 function libraryHandleClick(event) {
   event.preventDefault();
   refs.homeLink.classList.remove('current');
   refs.libraryBtn.classList.add('current');
+<<<<<<< Updated upstream
   // refs.header.classList.remove('header');
   // refs.header.classList.add('library');
+=======
+>>>>>>> Stashed changes
   updateHeaderMarkup(headerTemplates);
 
   if (document.querySelector('.modal')) {
@@ -35,6 +50,7 @@ function libraryHandleClick(event) {
   //const watchedBtn = document.querySelector('.header-button-watched');
   //const queueBtn = document.querySelector('.header-button-queue');
 
+<<<<<<< Updated upstream
   // refs.filmsGallery.innerHTML = '';
   // refs.paginationContainer.style.display = 'none';
   // updateFilmsLibraryMarkup(watchedFilms);
@@ -45,6 +61,17 @@ function libraryHandleClick(event) {
       updateFilmsLibraryMarkup();
       queueBtn.classList.remove('is-active-btn');
       watchedBtn.classList.add('is-active-btn');
+=======
+  refs.filmsGallery.innerHTML = '';
+  updateFilmsLibraryMarkup(watchedFilms);
+
+  function onLibraryButtonsClick(activeBtn, inactiveBtn) {
+    activeBtn.addEventListener('click', event => {
+      event.preventDefault();
+      updateFilmsLibraryMarkup();
+      inactiveBtn.classList.remove('is-active-btn');
+      activeBtn.classList.add('is-active-btn');
+>>>>>>> Stashed changes
     });
   }
 
@@ -56,7 +83,7 @@ function libraryHandleClick(event) {
   //onLibraryButtonsClick(queueBtn, watchedBtn, queuedFilms);
   //onLibraryButtonsClick(watchedBtn, queueBtn, watchedFilms);
 
-  function updateFilmsLibraryMarkup(localStorageFilms) {
+  function updateHeaderMarkup(localStorageFilms) {
     if (!localStorageFilms) {
       refs.filmsGallery.innerHTML = '';
       const message = '<div class="films-gallery-warning"><p>No movie</p><div>';
@@ -64,10 +91,30 @@ function libraryHandleClick(event) {
       return;
     }
 
+<<<<<<< Updated upstream
     //  refs.filmsGallery.innerHTML = '';
     //  localStorageFilms.map(({ id, poster_path, title, release_date, genres, vote_average }) => {
     //    const markup = genre2;
     //    refs.filmsGallery.insertAdjacentHTML('beforeend', markup);
     //  });
+=======
+    refs.filmsGallery.innerHTML = '';
+    localStorageFilms.map(({ id, poster_path, title, release_date, genres, vote_average }) => {
+      const markup = `
+<li class="films-gallery-item" data-id="${id}">
+  <img
+    class="films-gallery-item-image"
+    src="https://image.tmdb.org/t/p/w342${poster_path}"
+    alt="«${title}» film poster"
+  >
+  <p class="films-gallery-item-title">${title.toUpperCase()}</p>
+  <p class="films-gallery-item-info">${genres.join(
+    ', ',
+  )} | ${release_date}<span class="modal-info-vote-average library">${vote_average}</span></p>
+</li>
+`;
+      refs.filmsGallery.insertAdjacentHTML('beforeend', markup);
+    });
+>>>>>>> Stashed changes
   }
 }
