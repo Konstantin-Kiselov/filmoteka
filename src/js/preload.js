67f18c5cfd6b942437bc6f64321cc44fa8c4ee05
OlyaArta <artanika8@gmail.com>
2021-09-10 13:20:29 +0300

    add smooth scroll up and preload on my library link, home link

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,3 +1,12 @@
+//myLibraryLink = document.querySelector('.navigation__link');
+//myLibraryLink.addEventListener('click', onLoadPreloader);
+
 window.onload = function () {
   document.querySelector('.preloader').classList.add('preloader-remove');
 };
+
+// window.onload = onLoadPreloader();
+
+// function onLoadPreloader() {
+//   document.querySelector('.preloader').classList.add('preloader-remove');
+// }
