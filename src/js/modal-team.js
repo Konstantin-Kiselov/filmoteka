9e1e94f39ca9524c3a9efab50991a5e46b75faa0
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-09-15 03:29:01 +0300

    fixed modal in footer

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,64 +1,62 @@
-
 const refs = {
-    body: document.querySelector('body'),
-    modalTeamBackdrop: document.querySelector('.backdrop-team'),
-    openTeamLink: document.querySelector('.team'),
-    closeTeamBtn: document.querySelector('.modal-team-close-btn'),
-    modalTeam: document.querySelector('.modal-team'),
+  body: document.querySelector('body'),
+  modalTeamBackdrop: document.querySelector('.backdrop-team'),
+  openTeamLink: document.getElementById('js-team-modal'),
+  closeTeamBtn: document.querySelector('.modal-team-close-btn'),
+  modalTeam: document.querySelector('.modal-team'),
 };
 
 refs.openTeamLink.addEventListener('click', onModalTeamOpen);
 refs.closeTeamBtn.addEventListener('click', onModalTeamClose);
 
 function toggleModalTeam() {
-    window.addEventListener('keydown', onEscKeyPress);
-    refs.modalTeam.classList.toggle('is-hidden-team');
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.modalTeam.classList.toggle('is-hidden-team');
 }
 
 /////////////////////////////////
-function onModalTeamOpen() {
-    toggleModalTeam();
-    stopScroll();
+function onModalTeamOpen(e) {
+  e.preventDefault();
+  toggleModalTeam();
+  stopScroll();
 }
 
 function onModalTeamClose() {
-    toggleModalTeam();
-    onScroll();
+  toggleModalTeam();
+  onScroll();
 }
 
 // Закрытие модального окна по нажатию клавиши ESC
 function onEscKeyPress(event) {
-    const ESC_KEY_CODE = 'Escape';
-    //console.log(event.code);
-    const isEscKey = event.code === ESC_KEY_CODE;
-    if (isEscKey) {
-        toggleModalTeam();
-      onScroll();
-    }
+  const ESC_KEY_CODE = 'Escape';
+  //console.log(event.code);
+  const isEscKey = event.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    toggleModalTeam();
+    onScroll();
   }
-  
-  // Закрытие модального окна по клику на backdrop.
-  refs.modalTeam.addEventListener('click', onModalCloseBackdrop);
-  function onModalCloseBackdrop(evt) {
-    const isBackdrop = evt.target.classList.contains('backdrop-team');
-    // console.log(isBackdrop);
-    if (isBackdrop) {
-        toggleModalTeam();
-      onScroll();
-    }
+}
+
+// Закрытие модального окна по клику на backdrop.
+refs.modalTeam.addEventListener('click', onModalCloseBackdrop);
+function onModalCloseBackdrop(evt) {
+  const isBackdrop = evt.target.classList.contains('backdrop-team');
+  // console.log(isBackdrop);
+  if (isBackdrop) {
+    toggleModalTeam();
+    onScroll();
   }
+}
 //чтобы не скролился body под модалкой
 function stopScroll() {
-    const isBackdropIsHidden = refs.modalTeamBackdrop.classList.contains('is-hidden-team');
-    // console.log(isBackdropIsHidden);
-    if (!isBackdropIsHidden) {
-      refs.body.classList.add('no-scroll');
-    }
+  const isBackdropIsHidden = refs.modalTeamBackdrop.classList.contains('is-hidden-team');
+  // console.log(isBackdropIsHidden);
+  if (!isBackdropIsHidden) {
+    refs.body.classList.add('no-scroll');
+  }
 }
-  
+
 //чтобы скролилась страница после закрытия модального окна
 function onScroll() {
-    refs.body.classList.remove('no-scroll');   
+  refs.body.classList.remove('no-scroll');
 }
-  
-    
